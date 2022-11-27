e8a8d679226adc5f88a01d23cc6e344089164827
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-27 23:15:18 +0530

    custom server using node js, express, nodemon and postman

--- Express/app.js
+++ Express/app.js
@@ -4,12 +4,20 @@ const app = express();
 const port = 80;
 
 app.get("/", (req, res) => {
-    res.send("This is homepage express app with harry")
-})
+    res.status(200).send("This is homepage express app with harry")
+});
 app.get("/about", (req, res) => {
     res.send("This is aboutpage express app with harry")
-})
+});
+app.post("/about", (req, res) => {
+    res.send("This is aboutpage express app with harry")
+});
+app.get("/this", (req, res) => {
+    res.status(404).send("Page Not Found")
+});
 
 app.listen(port, ()=>{
     console.log(`App started successfully on port ${port}`)
-})
+});
+
+// #also installing nodemon from npm 
