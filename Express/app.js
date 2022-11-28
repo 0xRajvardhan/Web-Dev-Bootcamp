5ead29ca69b0c46166eeea46ce3a620bb1b6989b
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-28 16:25:02 +0530

    learnt about serving  static files using pug and template engine

--- Express/app.js
+++ Express/app.js
@@ -1,7 +1,21 @@
 const express = require("express")
-
+const path = require("path")
 const app = express();
 const port = 80;
+
+// For serving static files 
+app.use('/static', express.static('static'))
+
+// Setting the template engine as pug 
+app.set('view engine', 'pug')
+
+// Set the views directory 
+app.set('views', path.join(__dirname, 'views'))
+
+// Pug demo endpoint 
+app.get("/demo", (req, res) => {
+        res.status(200).render('demo', { title: 'Hey', message: 'Hello there, I am learning how to use pug!' })
+      });
 
 app.get("/", (req, res) => {
     res.status(200).send("This is homepage express app with harry")
