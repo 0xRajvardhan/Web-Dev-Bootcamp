fd3580ba018804650827aeb8038d883b04d1cd3c
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-30 01:57:13 +0530

    added index.pug

--- Express/app.js
+++ Express/app.js
@@ -3,34 +3,22 @@ const path = require("path")
 const app = express();
 const port = 80;
 
-// For serving static files 
-app.use('/static', express.static('static'))
+// EXPRESS RELATED CONFIG 
+app.use('/static', express.static('static')) // For serving static files 
 
-// Setting the template engine as pug 
-app.set('view engine', 'pug')
+// PUG RELATED CONFIG 
+app.set('view engine', 'pug') // Setting the template engine as pug 
+app.set('views', path.join(__dirname, 'views')) // Set the views directory 
 
-// Set the views directory 
-app.set('views', path.join(__dirname, 'views'))
+// ENDPOINTS
+app.get('/', (req, res) => {
+    const con = "This is the best content"
+    const params = {'title': 'pubg is the best game', 'content' : con }
+    res.status(200).render('index.pug', params)
+})
 
-// Pug demo endpoint 
-app.get("/demo", (req, res) => {
-        res.status(200).render('demo', { title: 'Hey', message: 'Hello there, I am learning how to use pug!' })
-      });
-
-app.get("/", (req, res) => {
-    res.status(200).send("This is homepage express app with harry")
-});
-app.get("/about", (req, res) => {
-    res.send("This is aboutpage express app with harry")
-});
-app.post("/about", (req, res) => {
-    res.send("This is aboutpage express app with harry")
-});
-app.get("/this", (req, res) => {
-    res.status(404).send("Page Not Found")
-});
-
-app.listen(port, ()=>{
+// START THE SERVER 
+app.listen(port, () => {
     console.log(`App started successfully on port ${port}`)
 });
 
