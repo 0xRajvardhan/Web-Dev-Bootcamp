09b67cff78ed3ea23f69d8b4b75e54ed70394e35
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-11-30 18:49:42 +0530

    syntax fixes

--- Express/app.js
+++ Express/app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path")
+const path = require("path");
 const app = express();
 const port = 80;
 
 // EXPRESS RELATED CONFIG 
 app.use('/static', express.static('static')) // For serving static files 
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 
 // PUG RELATED CONFIG 
-app.set('view engine', 'pug') // Setting the template engine as pug 
-app.set('views', path.join(__dirname, 'views')) // Set the views directory 
+app.set('view engine', 'pug'); // Setting the template engine as pug 
+app.set('views', path.join(__dirname, 'views')); // Set the views directory 
 
 // ENDPOINTS
 app.get('/', (req, res) => {
     const con = "This is the best content"
     const params = { 'title': 'pubg is the best game', 'content': con }
-    res.status(200).render('index.pug', params)
+    res.status(200).render('index.pug', params);
 })
 
 
@@ -29,17 +29,17 @@ app.post('/', (req, res) => {
 
     let outputToWrite = `The name of our client is ${name},\n${age} years, \n${gender}, \nResiding in ${address}. \nMore about them: ${more}`
 
-    fs.writeFileSync('output.txt', outputToWrite)
+    fs.writeFileSync('output.txt', outputToWrite);
     
     const params = { 'message': 'Your form has been submitted successfully'}
-    res.status(200).render('index.pug', params)
+    res.status(200).render('index.pug', params);
 })
 
 
 
 // START THE SERVER 
 app.listen(port, () => {
-    console.log(`App started successfully on port ${port}`)
+    console.log(`App started successfully on port ${port}`);
 });
 
 // #also installing nodemon from npm 
