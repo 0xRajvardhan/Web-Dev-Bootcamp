baa86d96683358502f77c377d56e7404bd0644aa
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-04 23:35:57 +0530

    mongo

---
+++ tut81.js
@@ -0,0 +1,9 @@
+// Inseting data in mongo db
+
+use harryKart
+
+db.item.insertOne({name:"Samsung 30s", price: 22000, rating: 4.5, qty: 233, sold: 98})
+
+db.item.insertMany([{name:"Samsung 10s", price: 32000, rating: 4.0, qty: 199, sold: 50}, {name:"Moto 30s", price: 12000, rating: 3.5, qty: 133, sold: 84}])
+
+db.items.find()  // If you want to see the date
