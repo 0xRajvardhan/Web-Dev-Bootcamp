69e6af663202473d7f81058d4b234bbc77c545ac
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-05 22:40:43 +0530

    mongodb

--- tut81.js
+++ tut81.js
@@ -7,3 +7,5 @@ db.item.insertOne({name:"Samsung 30s", price: 22000, rating: 4.5, qty: 233, sold
 db.item.insertMany([{name:"Samsung 10s", price: 32000, rating: 4.0, qty: 199, sold: 50}, {name:"Moto 30s", price: 12000, rating: 3.5, qty: 133, sold: 84}])
 
 db.items.find()  // If you want to see the date
+
+db.dropDatabase() // Deletes current database you are working on
